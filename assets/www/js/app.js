09ca7d15acda773cb018df708f01c1e141eba15b
Charlie Coglianese <charles.coglianese@gmail.com>
2011-11-14 19:17:07 -0700

    Working versions of recipe and makeit pages.

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -1,56 +1,68 @@
-// 
-//  --- our app behavior logic ---
-//
-run(function () {
-    // immediately invoked on first run
-    var init = (function () {
-        if (navigator.network && navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
-        } else {
-//            alert("We can reach Google - get ready for some awesome maps!");
-        }
-    })();
-    
-    // a little inline controller
-    when('#welcome');
-    when('#settings', function() {
-		// load settings from store and make sure we persist radio buttons.
-		store.get('config', function(saved) {
-			if (saved) {
-				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
-				}
-				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
-				}
-			}
-		});
+$('#indexPage').live('pageinit', function(event) {
+	$('#login').click(function() {
+		window.location = "./search.html";	
 	});
-    when('#map', function () {
-        store.get('config', function (saved) {
-            // construct a gmap str
-            var map  = saved ? saved.map || ui('map') : ui('map')
-            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
-
-                x$('img#static_map').attr('src', path);
-            }, function () {
-                x$('img#static_map').attr('src', "img/gpsfailed.png");
-            });
-        });
-    });
-    when('#save', function () {
-        store.save({
-            key:'config',
-            map:ui('map'),
-            zoom:ui('zoom')
-        });
-        display('#welcome');
-    });
+	
+	//					$.getJSON('http://api.alternativeto.net/software/'
+	//							+$('#searchBox').val()+'/?count=15', 
+	//						function(data) {
+	//							var items = data.Items;
+	//							var list = $('#list');
+	//							list.html("");
+	//							$.each(items, function(key, val) {
+	//								list.append($(document.createElement('li')).html(val.Name));
+	//							});
+	//							list.listview("destroy").listview()
+	//						});
+	document.addEventListener("deviceready", onDeviceReady, false);
 });
+
+$('#searchPage').live('pageinit', function(event) {
+	$('#searchButton').click(function() {
+		//$.getJSON('http://api.alternativeto.net/software/'
+		//		+$('#searchBox').val()+'/?count=15', 
+		//	function(data) {
+		//		var items = data.Items;
+				var list = $('#list');
+				list.html("");
+				//$.each(items, function(key, val) {
+				list.append($(document.createElement('li')).attr('data-theme', 'c').html('<a href="#recipePage">Kale Salad</a>'));
+				list.append($(document.createElement('li')).attr('data-theme', 'c').html('<a href="#recipePage">Red Kale Salad</a>'));
+				list.append($(document.createElement('li')).attr('data-theme', 'c').html('<a href="#recipePage">Kale and White Bean Soup</a>'));
+				//});
+				list.listview("destroy").listview();
+	});
+});
+
+$('#makeitPage').live('pageinit', function(event) {
+	var currYear = (new Date()).getFullYear();
+	var datePickerOptions = { preset: 'datetime', theme: 'ios', mode: 'scroller',
+		setText: 'Done', startYear: currYear, endYear: currYear + 1, hourText: 'Hour', minuteText: 'Minute',
+		dateFormat: 'D M d, yy', timeFormat: 'h:ii A' }; 
+	$('#date0').scroller(datePickerOptions);
+    $('#date1').scroller(datePickerOptions);
+    $('#date2').scroller(datePickerOptions);
+    $('#date3').scroller(datePickerOptions);
+	$('#setreminders').click(function() {
+		$.mobile.changePage("#recipePage", {
+			transition: "slide",
+			reverse: true,
+			changeHash: true
+		});	
+	});
+});
+
+function reachableCallback(reachability) {
+	// There is no consistency on the format of reachability
+	var networkState = reachability.code || reachability;
+	var states = {};
+	states[NetworkStatus.NOT_REACHABLE]                      = 'No network connection';
+	states[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'Carrier data connection';
+	states[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK]         = 'WiFi connection';
+	alert('Connection type: ' + states[networkState]);
+}
+
+// PhoneGap is loaded and it is now safe to make calls PhoneGap methods
+function onDeviceReady() {
+	navigator.network.isReachable('phonegap.com', reachableCallback);
+}
